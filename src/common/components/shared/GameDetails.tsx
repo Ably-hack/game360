2487f60db41f6aebf397334199fdcb8e47686c0e
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2023-10-08 10:47:57 +0100

    Create base components and MatchCard

---
+++ src/common/components/shared/GameDetails.tsx
@@ -0,0 +1,49 @@
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+import useMediaQuery from 'src/common/hooks/useMediaQuery';
+import { GameDetails } from 'src/types/GameDetails';
+
+const inter = Inter({ subsets: ['latin'] });
+
+function GameDetails({
+  stadium = 'Estádio do Dragão',
+  location = 'Porto, Portugal',
+}: GameDetails) {
+  const isMobile = useMediaQuery('(min-width: 430px)');
+
+  return (
+    <div
+      className={` p-2 flex justify-between border border-[#EDEDED]  bg-white ${inter.className}`}>
+      <div className="flex">
+        <Image
+          className=""
+          src="/images/location_pin.svg"
+          height={32}
+          width={32}
+          alt={'location'}
+        />
+        <div className="text-sm">
+          <p className=" my-2 text-center text-neutral-900 font-medium">
+            {stadium}
+          </p>
+          {isMobile && <p className=" text-gray-400">{location}</p>}
+        </div>
+      </div>
+
+      <div className="text-[#1A1B1C] flex items-center text-sm">
+        <Link href="#" className="px-4 hover:underline">
+          Summary
+        </Link>
+
+        <Link
+          href="#"
+          className="px-4 border-l border-[#EDEDED] hover:underline">
+          Statistics
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default GameDetails;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/flowbite-react/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        'game360-gray': '#888888',
+        'game360-red': '#DD0000',
+        'game360-neutral-900': '#1A1B1C',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require('flowbite/plugin')],
 };
-export default config
+export default config;
