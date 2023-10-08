04671f92ca24fdc3a91880ae826394c28f38f4db
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2023-10-08 12:23:53 +0100

    Resolve merge conflicts during rebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import DashboardLayout from 'src/common/components/layout/DashboardLayout';
 
 export default function Home() {
@@ -6,17 +5,5 @@ export default function Home() {
     <DashboardLayout>
       <div className="text-neutral-400">Jus here</div>
     </DashboardLayout>
-=======
-import MatchCardList from 'src/common/components/dashboard/home/MatchCardList';
-
-// const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
-  return (
-    <>
-      {/* <MatchCard /> */}
-      <MatchCardList />
-    </>
->>>>>>> d66b0b3ed6da263d22fec13b3eefc308e37777b3
   );
 }
