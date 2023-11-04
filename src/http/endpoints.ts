2ecb227ce8a12a953cfee1c21e7d14d3c9af488d
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-11-04 17:22:43 +0100

    http: api service ~endpoints

---
+++ src/http/endpoints.ts
@@ -0,0 +1,18 @@
+class EndPoints {
+  static API_VERSION = 'v1';
+  static LOCAL_BASE_ROOT = 'http://localhost:8000';
+  static REMOTE_BASE_ROOT = 'https://game360-backend.vercel.app';
+  static LOCAL_BASE = `${this.LOCAL_BASE_ROOT}/${this.API_VERSION}`;
+  static REMOTE_BASE = `${this.REMOTE_BASE_ROOT}/${this.API_VERSION}`;
+  static BASE_URL = this.REMOTE_BASE;
+  static REGISTER = `${this.BASE_URL}/account/register`;
+  static LAUNCHPAD_CREATE_PACKAGE = `${this.BASE_URL}/launchpad/create`;
+  static UPLOAD_IMAGE = `${this.BASE_URL}/files/upload`;
+  static FETCH_ALL_PROJECTS = `${this.BASE_URL}/project/all`;
+  static FETCH_PROJECT_DETAIL = `${this.BASE_URL}/project/:id`;
+  static CREATE_PROFILE = `${this.BASE_URL}/account/profile/create`;
+  static FETCH_PROFILE = `${this.BASE_URL}/account/profile/:address`;
+  static UPSERT_PROFILE = `${this.BASE_URL}/account/profile/upsert`;
+}
+
+export default EndPoints;
