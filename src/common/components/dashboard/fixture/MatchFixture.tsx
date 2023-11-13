c5cbdf314dbe55089e807ff648307a5a02645923
KcPele <fidekg123@gmail.com>
2023-11-13 20:17:23 +0100

    added team stat

--- src/common/components/dashboard/fixture/MatchFixture.tsx
+++ src/common/components/dashboard/fixture/MatchFixture.tsx
@@ -5,28 +5,31 @@ import LiveChat from './components/LiveChat';
 import PredictionBar from './components/PredictionBar';
 import Record from './components/Record';
 import {
- PREDICTION_DATA,
- ARSENAL_LAST_GAME_RECORD_DATA,
- CHELSEA_LAST_GAME_RECORD_DATA,
- HEAD_TO_HEAD_RECORD_DATA,
+  PREDICTION_DATA,
+  ARSENAL_LAST_GAME_RECORD_DATA,
+  CHELSEA_LAST_GAME_RECORD_DATA,
+  HEAD_TO_HEAD_RECORD_DATA,
 } from 'src/common/constants';
+import TeamStatistics from './components/TeamStatistics';
 function MatchFixture() {
   return (
     <div className="grid grid-cols-12 gap-1">
       <div className="col-span-full md:col-span-4 lg:col-span-4 xl:col-span-3 mt-1">
         {/* left section */}
-        <AboutMatch/>
-        <ChampionsNews/>
+        <AboutMatch />
+        <ChampionsNews />
       </div>
       <div className="col-span-full md:col-span-4 lg:col-span-4 xl:col-span-6 mt-1">
         {/* Middle section  */}
         <PredictionBar
-            firstClub={PREDICTION_DATA[0]}
-            secondClub={PREDICTION_DATA[1]}
-          />
-          <Record data={HEAD_TO_HEAD_RECORD_DATA} />
-           <Record data={CHELSEA_LAST_GAME_RECORD_DATA} />
-          <Record data={ARSENAL_LAST_GAME_RECORD_DATA} />
+          firstClub={PREDICTION_DATA[0]}
+          secondClub={PREDICTION_DATA[1]}
+        />
+        <TeamStatistics />
+
+        <Record data={HEAD_TO_HEAD_RECORD_DATA} />
+        <Record data={CHELSEA_LAST_GAME_RECORD_DATA} />
+        <Record data={ARSENAL_LAST_GAME_RECORD_DATA} />
 
         <div className="grid gap-1"></div>
       </div>
